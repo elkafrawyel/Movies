c54216253abc2f37e946ef11b3bf6f615fe37058
elfiky <elfiky-work@hmaserv.com>
2019-06-08 12:42:34 +0200

    initial commit

--- app/src/main/java/com/apps/elkafrawyel/movies_tv/FirebaseService/MyFirebaseMessagingService.java
+++ app/src/main/java/com/apps/elkafrawyel/movies_tv/FirebaseService/MyFirebaseMessagingService.java
@@ -6,6 +6,5 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-
     }
 }
